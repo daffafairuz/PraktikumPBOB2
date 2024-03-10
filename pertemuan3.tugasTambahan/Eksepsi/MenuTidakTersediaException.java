a1bb94270f4da546bc0509fe4b41f0b2d6b14cf6
daffafairuz <126538988+daffafairuz@users.noreply.github.com>
2024-03-10 15:03:47 +0700

    Add files via upload

---
+++ pertemuan3.tugasTambahan/Eksepsi/MenuTidakTersediaException.java
@@ -0,0 +1,8 @@
+public class MenuTidakTersediaException extends Exception {
+    // TO DO 2: Buat konstructor dengan parameter message dan panggil konstruktor parent dengan keyword super
+    public MenuTidakTersediaException(String message){
+        super(message);
+    }    
+}
+
+
